d1e48d17ade8c6b9e40d9bc82582274212863957
Otzar Jaffe <otzarjaffe@gmail.com>
2021-04-17 12:26:45 -0400

    adding exclaims

--- Final Website Files/Javascript-files/home.js
+++ Final Website Files/Javascript-files/home.js
@@ -5,6 +5,6 @@
 let response = confirm("Elect Otzar as President!");
 
 if (response == null) {
-    alert(`You should really consider electing Otzar as President. He has really great ideas for our class!`);
+    alert(`You should really consider electing Otzar as President. He has really great ideas for our class!!!`);
   }
 }
