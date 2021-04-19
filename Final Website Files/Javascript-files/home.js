3f2bd46512eda4d3c74ac70ffb903853e1c0de95
Otzar Jaffe <otzarjaffe@gmail.com>
2021-04-19 15:50:39 -0400

    uploading javascript file

--- Final Website Files/Javascript-files/home.js
+++ Final Website Files/Javascript-files/home.js
@@ -1,11 +1,9 @@
 //JS file
-/*
+
 "use strict";
 
 let response = confirm("Elect Otzar as President!");
 
-if (response == null) {
-    alert(`You should really consider electing Otzar as President. He has really great ideas for our class!!!`);
-  }
+if (response === false) {
+  alert("You really should elect Otzar as President. He has great ideas!");
 }
-*/
